3668992a15042b3f5a70ea2a12d065e3ab4475e0
djghis <djghis@hotmail.com>
2020-01-29 18:19:11 +0000

    delete_one function added

--- music_collection/models/Album.rb
+++ music_collection/models/Album.rb
@@ -41,6 +41,12 @@ class Album
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE FROM albums WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * FROM albums"
     albums = SqlRunner.run(sql)
--- music_collection/models/Artist.rb
+++ music_collection/models/Artist.rb
@@ -29,6 +29,12 @@ class Artist
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE FROM artists WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end 
+
   def self.delete_all()
 
     sql = "DELETE FROM artists"
